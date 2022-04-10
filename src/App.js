64b809726f07e76325814440f222241f36e4e62c
begmamatov <shahzodjonyagzon@gmail.com>
2022-04-10 20:54:58 +0500

    react app firebase auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
+// import './App.css';
+// import Header from './components/Header'
+// import Sidebar from './components/Sidebar';
+// import Mail from './components/Mail';
+// import MailList from './components/MailList';
+// import SendMail from "./components/SendMail";
+// import { useSelector } from "react-redux";
+// import { selectSendMessageIsOpen } from "./features/mailSlice";
 
+// function App() {
+//   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
+
+//   return (
+//     <BrowserRouter className="App">
+//       <div>
+//         <Header />
+
+//         <div className="app_body">
+//           <Sidebar />
+
+//           <Routes>
+//             <Route path='/mail' element={<Mail />} />
+//             <Route path='/' element={<MailList />} />
+//           </Routes>
+//         </div>
+
+//         {sendMessageIsOpen && <SendMail />}
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Reset from "./Reset";
+import Dashboard from "./Dashboard";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
